cce34a18da2edf3d11e0fea0ef6aff1f3492c6a5
Ariq Fikri Tamamy <ariqrick@gmail.com>
2024-10-01 22:18:13 +0700

    Pengumpulan Tugas 6

---
+++ tugas-reactjs/src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+
+function App() {
+  let nama = 'Ariq';
+  return (
+    <div className="App">
+      <div className='title'>
+        <h1>Hi, Saya {nama}</h1>
+        <hr></hr>
+        <p>Saya dari Batch 36 sudah berhasil menginstall React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
